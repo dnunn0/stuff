813acc1a275530446848a6b05ad2a40fd36cd7ae
David Nunn <david.p.nunn@gmail.com>
2017-05-07 03:09:45 -0700

    createDistribution.bat

--- createDistribution.bat
+++ createDistribution.bat
@@ -1,6 +1,6 @@
 setlocal
-if not "%JRE_HOME%"=="" goto JRE_SET
-Echo JRE_HOME not set
+if not "%JAVA_HOME%"=="" goto JRE_SET
+Echo JAVA_HOME not set
 goto :eof
 :JRE_SET
 
@@ -9,12 +9,12 @@ for %%* in (.) do set app_name=%%~nx*
 
 Set ROBOCOPY_OK=3
 CALL :DELETE_DIR %app_dir%dist
-robocopy "%JRE_HOME%" "%app_dir%dist\%app_name%\runtime\jre" /E /PURGE /COPY:DAT /DCOPY:T /NS /NC /NFL /NDL /NP
+robocopy "%JAVA_HOME%\jre" "%app_dir%dist\%app_name%\runtime\jre" /E /PURGE /COPY:DAT /DCOPY:T /NS /NC /NFL /NDL /NP
 IF %ERRORLEVEL% GTR %ROBOCOPY_OK% goto done
 
 del "%app_dir%dist\%app_name%\runtime\jre"\Welcome.html
 
-set FILE_LIST=(charsets deploy jce jsse resources )
+set FILE_LIST=(charsets deploy jce resources jfr jfswt plugin management-agent )
 for %%i in %FILE_LIST% do del "%app_dir%dist\%app_name%\runtime\jre"\lib\%%i.jar 
 CALL :DELETE_DIR "%app_dir%dist\%app_name%\runtime\jre"\lib\ext
 CALL :DELETE_DIR del /s/q "%app_dir%dist\%app_name%\runtime\jre"\lib\management
@@ -42,13 +42,29 @@ rm *.zip
 rm *.tar
 popd
 
-set batfile= dist\%app_name%\run.bat
+set batchfile= dist\%app_name%\run.bat
 
-Echo setlocal > %batfile%
-Echo Set app_dir=%%~dp0 >> %batfile%
-Echo pushd %%app_dir%% >> %batfile%
-Echo set PATH=runtime\jre\bin >> %batfile%
-Echo app\bin\%app_name% %%*>> %batfile%
+Echo setlocal> %batchfile%
+Echo Set app_dir=%%~dp0>> %batchfile%
+Echo pushd %%app_dir%% >> %batchfile%
+Echo Set JAVA_HOME=%%app_dir%%runtime\jre\>> %batchfile%
+Echo set PATH=%%JAVA_HOME%%bin>> %batchfile%
+Echo Set JAVA_OPTS=-server -Xmx8m >>%batchfile%
+Echo Set DEBUG=y >>%batchfile%
+Echo app\bin\%app_name% %%* >> %batchfile%
+
+set batchfile= dist\%app_name%\run
+
+Echo #!/bin/bash>> %batchfile%
+Echo app_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )">> %batchfile%
+Echo pushd $app_dir>> %batchfile%
+Echo export JAVA_HOME=${app_dir}/runtime/jre/>> %batchfile%
+Echo export PATH=${JAVA_HOME}bin:/usr/bin:/bin>> %batchfile%
+Echo export JAVA_OPTS='-server -Xmx8m' >> %batchfile%
+Echo export DEBUG=y >> %batchfile%
+Echo chmod 777 app/bin/%app_name% >> %batchfile%
+Echo chmod 777 runtime/bin/* >> %batchfile%
+Echo app/bin/%app_name% $ *>> %batchfile%
 
 pushd dist && "C:\Program Files (x86)\7-Zip\7z" a -tzip -mx7 %app_name% && popd
 
