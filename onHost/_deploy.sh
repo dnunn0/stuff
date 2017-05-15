6521ca51ee3ada22df0b8404cfe8c7e369370c10
David Nunn <david.p.nunn@gmail.com>
2017-05-15 15:56:56 -0700

    better logging in deploy

--- onHost/_deploy.sh
+++ onHost/_deploy.sh
@@ -6,10 +6,11 @@ set -x
 cmd //c "robocopy /J \\\\tsclient\C\WithAws %TEMP%"
 unzip /tmp/firefly -d c:/firefly-wait
 
-
-# ----------------- Kill Old Version -----------------
+echo -->/dev/null
+echo ----------------- Kill Old Version ----------------- > /dev/null
 date
-# ----------------------------------------------------
+echo ---------------------------------------------------->/dev/null
+echo -->/dev/null
 
 cmd //c "taskkill /f /im java*"
 # doesn't kill java process kill $(ps -e | grep "firefly/run.bat"| awk '{print $1}')
@@ -18,7 +19,9 @@ sleep 1s
 rm -r /c/firefly
 mv /c/firefly-wait/firefly /c/firefly
 rm -r /c/firefly-wait
-# ===================== New Version ========================
+echo ==>/dev/null
+echo ===================== New Version ========================>/dev/null
 date
-# ==========================================================
+echo ==========================================================>/dev/null
+echo ==>/dev/null
 /c/firefly/run.bat 24680 KALIDASA >>$logfile 2>&1
