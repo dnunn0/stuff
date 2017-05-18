a9d1dd6ae6783e064fd2fdd982162cd67d35621b
David Nunn <david.p.nunn@gmail.com>
2017-05-18 14:38:11 -0700

    more logging of request ip:port

--- src/main/java/com/whatgameapps/firefly/controller/BeforeAll.java
+++ src/main/java/com/whatgameapps/firefly/controller/BeforeAll.java
@@ -12,9 +12,13 @@ public class BeforeAll {
     public BeforeAll(Service spark) {
         spark.before((Request req, Response res) -> {
             res.header(NavController.ID_HEADER, System.currentTimeMillis() + ":" + id.incrementAndGet());
-            System.out.format("\n %TD:%<TT - %s - Received request for %s %s\n",
+            String ipAddress = req.raw().getHeader("X-FORWARDED-FOR");
+            if (ipAddress == null) {
+                ipAddress = String.format("%s:%s", req.raw().getRemoteAddr(), req.raw().getRemotePort());
+            }
+            System.out.format("\n %TD:%<TT - %s - Received request for %s %s from %s\n",
                     System.currentTimeMillis(), res.raw().getHeader(NavController.ID_HEADER),
-                    req.requestMethod(), req.uri());
+                    req.requestMethod(), req.uri(), ipAddress);
                 }
         );
     }
